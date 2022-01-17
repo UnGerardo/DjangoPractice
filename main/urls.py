277a428b899ab3911329ccd8a1281e591017d2f0
UnGerardo <gerardojdls@gmail.com>
2022-01-17 08:45:24 -0800

    Created new models and registered them in admin.py, created new template allLists and added a route to it and it shows all lists

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import index
+from .views import index, allLists
 
 urlpatterns = [
     path("", index, name="Index"),
+    path("all/", allLists, name="All Lists"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ToDoList
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {})
+
+def allLists(request):
+    listObjects = ToDoList.objects
+    return render(request, "allLists.html", {"listObjects": listObjects})
