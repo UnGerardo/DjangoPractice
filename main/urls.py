25a12670ca36baf55452e29d23e402e98c07586d
UnGerardo <gerardojdls@gmail.com>
2022-01-17 08:57:27 -0800

    Create listById.html which takes in an id in the route as an arg and the view return the list with that id

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, allLists
+from .views import index, allLists, listById
 
 urlpatterns = [
     path("", index, name="Index"),
     path("all/", allLists, name="All Lists"),
+    path("list/<int:id>", listById, name="List By Id"),
 ]
--- main/views.py
+++ main/views.py
@@ -8,3 +8,7 @@ def index(request):
 def allLists(request):
     listObjects = ToDoList.objects
     return render(request, "allLists.html", {"listObjects": listObjects})
+
+def listById(request, id):
+    todolist = ToDoList.objects.get(id=id)
+    return render(request, "listById.html", {"todolist": todolist})
