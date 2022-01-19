beb36c4c398c21ed6f5f1c5a47b629619c7def98
UnGerardo <gerardojdls@gmail.com>
2022-01-19 15:33:54 -0800

    Created registration and login page, modified necessary files for routing and blocks, also changed login/out redirects

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, allLists, listById, createTable
 
 urlpatterns = [
-    path("", index, name="Index"),
+    path("home/", index, name="Index"),
     path("all/", allLists, name="All Lists"),
     path("list/<int:id>", listById, name="List By Id"),
     path("create/", createTable, name="Create Table"),
--- registration/urls.py
+++ registration/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/", register, name="Register"),
