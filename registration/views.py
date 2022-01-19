8d48b1a2e2ff12bcb0adc86c05f24bf0c447df77
UnGerardo <gerardojdls@gmail.com>
2022-01-19 14:47:41 -0800

    Users can now register their account

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username = form.cleaned_data["username"],
+                email = form.cleaned_data["email"],
+                password = form.cleaned_data["password1"]
+            )
+            user.save()
+            # return HttpResponseRedirect("/register/")
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
